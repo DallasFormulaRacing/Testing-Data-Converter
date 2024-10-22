456d73df7387b3d19a728889e126f437c87f4ad6
andrewklundtFT <andrew.klundt@utdallas.edu>
2024-10-22 01:24:30 -0500

    Replaced the parser and adjusted steering range for degrees
    I replaced ldparser.py with a new python file that can write .ld files better. With the new writer, we can configure units for each channel.

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
+from mandrewLDWriter import File, Channel
 import pandas as pd
 import process_steered_angle
 import interpolate_data
-import ldparser
+import time
 
 # Load CSV file into a dataframe
 steered_angle_dataframe = interpolate_data.csv_to_dataframe('Data/random_steering_data_from_grafana_10-13-2024.csv')
@@ -13,5 +14,41 @@ ranged_steered_angle_dataframe = pd.DataFrame({'Time': steered_angle_dataframe['
 # for val in ranged_steered_angle_dataframe['Steered Angle']:
 #     print(f'{val},', end='')
 
-ld_data = ldparser.ldData.frompd(ranged_steered_angle_dataframe)
-ld_data.write('output.ld')
+
+# Create a File instance
+example_file = File()
+example_file.Time = time.localtime()
+example_file.Driver = 'Reid Minton'
+example_file.Vehicle = 'Baller Mobile'
+example_file.Venue = 'Pedestrian Promenade'
+example_file.ShortComment = 'A brief ride through the pedestrian promenade'
+example_file.EventName = 'Pedestrian Promenade Grand Prix'
+example_file.EventSession = 'Qualifying'
+example_file.EventComment = 'event comment here'
+example_file.VehicleId = '1234'
+example_file.VehicleWeight = 300
+example_file.VehicleType = 'Formula Car'
+example_file.VehicleComment = 'Super fast Dallas Formula Racing car'
+
+# Create channels
+channel1 = Channel(
+    frequency=98,
+    name='Steered Angle',
+    short_name='SA',
+    unit='deg',
+    data=ranged_steered_angle_dataframe['Steered Angle']
+)
+
+# channel2 = Channel(
+#     frequency=100,
+#     name='Speed',
+#     short_name='SPD',
+#     unit='km/h',
+#     data=[0.0, 50.0, 100.0, 150.0]
+# )
+
+# Add channels to the file
+example_file.add_channels(channel1)
+
+# Write the file
+example_file.write(open('output.ld', 'wb'))
--- process_steered_angle.py
+++ process_steered_angle.py
@@ -9,4 +9,6 @@ def process_steered_angle(series):  # dataframe column
     # Scale the data to fit within the bounds [-10,10] like MoTec steered angle
     angle_series = angle_series.apply(lambda x: x * 8.888 if x > 0 else x * 6.7114094 if x < 10 else x)
     angle_series = angle_series.apply(lambda x: 10 if x > 10 else -10 if x < -10 else x)
+    # Convert to MoTec's degrees range. It's weirdly automatically between -572.9578 and 572.9578
+    angle_series = angle_series.apply(lambda x: x * 57.29578)
     return angle_series

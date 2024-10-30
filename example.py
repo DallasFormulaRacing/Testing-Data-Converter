7a799631cffcb45924329005b9a63b1dee6be823
Andrew Klundt <andrew.klundt@utdallas.edu>
2024-10-29 20:12:28 -0500

    Renamed channels to match MoTec
    Renamed most of the channels to match those in MoTec's template workstations.
    Switched the driver's name.

--- example.py
+++ example.py
@@ -13,17 +13,6 @@ def process_channel(dataframe, time_series_name, data_series_name, transform_fun
     return tc.assimilate_channel(time_series, data_series)
 
 
-# Creates a channel object with the given parameters
-def create_channel(name, short_name, unit, data, frequency):
-    return Channel(
-        frequency=frequency,
-        name=name,
-        short_name=short_name,
-        unit=unit,
-        data=data
-    )
-
-
 if __name__ == "__main__":
     # Load the data from the CANbus CSV file
     can_dataframe = pd.read_csv('Data/Converted CANbus Data/758.csv')
@@ -32,30 +21,32 @@ if __name__ == "__main__":
     channels_info = [
         # (Name, Short Name, Unit, Data Series Name, Transform Function)
         ('Engine RPM', 'RPM', 'rpm', 'RPM', None),
-        ('Wheel Speed', 'WS', 'ft/s', 'Driven Wheel Speed #1', None),
-        ('Eng Oil Pres', 'OP', 'V', 'Analog Input #1', None),
-        ('Front Brakes Pres', 'BPF', 'psi', 'Analog Input #6', tc.transform_brakes_pressure),
-        ('Rear Brakes Pres', 'BPR', 'psi', 'Analog Input #4', tc.transform_brakes_pressure),
+        ('Wheel Speed RL', 'WSRL', 'ft/s', 'Driven Wheel Speed #1', None),
+        ('Eng Oil Pres', 'EOP', 'V', 'Analog Input #1', None),  # This is volts ??? I think this is wrong
+        ('Brake Pres Front', 'BPF', 'psi', 'Analog Input #6', tc.transform_brakes_pressure),
+        ('Brake Pres Rear', 'BPR', 'psi', 'Analog Input #4', tc.transform_brakes_pressure),
         ('Steered Angle', 'SA', 'deg', 'Analog Input #2', tc.transform_steered_angle),
         ('Throttle Pos', 'TP', '%', 'TPS', None),
-        ('Inlet Air Temp', 'IAT', 'C', 'Air Temp', None),
-        ('Inlet Manifold Pres', 'IMP', 'kPa', 'MAP', None),
-        ('Exhaust Lambda', 'EL', 'LA', 'Lambda', None),
-        ('Ignition Timing', 'IT', 'deg', 'Ignition Angle', None),
-        ('ECU Battery Voltage', 'EBV', 'V', 'Battery Volt', None),
+        ('Air Temp Inlet ', 'ATI', 'C', 'Air Temp', None),
+        ('Manifold Pres', 'MP', 'kPa', 'MAP', None),
+        ('Lambda 1', 'L1', 'LA', 'Lambda', None),
+        ('Ign Advance', 'IA', 'deg', 'Ignition Angle', None),  # This name is a guess
+        ('Battery Volts', 'BV', 'V', 'Battery Volt', None),
         # Here are the channels that weren't in Reid's original list
-        ('Fuel Open Time', 'FOT', 'ms', 'Fuel Open Time', None),
-        ('Coolant Temp', 'CT', 'C', 'Coolant Temp', None),
+        ('Fuel Effective PW', 'FEP', 'ms', 'Fuel Open Time', None),  # This name is a guess
+        ('Coolant Temp', 'CT', 'C', 'Coolant Temp', None),  # Couldn't find equivalent in MoTec samples
         ('Frequency 1', 'F1', 'Hz', 'Frequency 1', None),
         ('Frequency 2', 'F2', 'Hz', 'Frequency 2', None),
         ('Frequency 3', 'F3', 'Hz', 'Frequency 3', None),
         ('Frequency 4', 'F4', 'Hz', 'Frequency 4', None),
-        ('Driven Avg Wheel Speed', 'DAWS', 'ft/s', 'Driven Avg Wheel Speed', None),
-        ('Non-Driven Avg Wheel Speed', 'NDAWS', 'ft/s', 'Non-Driven Avg Wheel Speed', None),
-        ('Ignition Compensation', 'IC', 'deg', 'Ignition Compensation', None),
-        ('Ignition Cut Percentage', 'ICP', '%', 'Ignition Cut Percentage', None),
-        ('Driven Wheel Speed #2', 'DWS2', 'ft/s', 'Driven Wheel Speed #2', None),
-        ('Barometer', 'BAR', 'kPa', 'Barometer', None),
+        ('Wheel Speed Rear', 'WSR', 'ft/s', 'Driven Avg Wheel Speed', None),
+        ('Wheel Speed RR', 'WSRR', 'ft/s', 'Driven Wheel Speed #2', None),
+        ('Wheel Speed Front Average', 'WSFA', 'ft/s', 'Non-Driven Avg Wheel Speed', None),
+        ('Wheel Speed Front Left', 'WSFL', 'ft/s', 'Non-Driven Wheel Speed #1', None),
+        ('Wheel Speed Front Right', 'WSFR', 'ft/s', 'Non-Driven Wheel Speed #2', None),
+        ('Ignition Compensation', 'IC', 'deg', 'Ignition Compensation', None),  # Couldn't find equivalent in MoTec samples
+        ('Ign Cut Level Total', 'ICLT', '%', 'Ignition Cut Percentage', None),
+        ('Baro Pres', 'BP', 'kPa', 'Barometer', None),
         ('Analog Input #1', 'AI1', 'V', 'Analog Input #1', None),
         ('Analog Input #3', 'AI3', 'V', 'Analog Input #3', None),
         ('Analog Input #5', 'AI5', 'V', 'Analog Input #5', None),
@@ -66,7 +57,7 @@ if __name__ == "__main__":
     # Create the MoTeC file object
     motec_file = File()
     motec_file.Time = time.localtime(can_dataframe['timestamp'][0])
-    motec_file.Driver = 'Reid Minton'
+    motec_file.Driver = 'Andrew is so cool'
     motec_file.Vehicle = 'Baller Mobile'
     motec_file.Venue = 'Pedestrian Promenade'
     motec_file.ShortComment = 'A brief ride through the pedestrian promenade'
@@ -81,7 +72,7 @@ if __name__ == "__main__":
     # Loop through the channels, process them, and add them to the MoTeC file
     for name, short_name, unit, data_series_name, transform_function in channels_info:
         data, frequency = process_channel(can_dataframe, 'timestamp', data_series_name, transform_function)
-        channel = create_channel(name, short_name, unit, data, frequency)
+        channel = Channel(frequency, name, short_name, unit, data)
         motec_file.add_channels(channel)
 
     # Write the MoTeC file to the output.ld file
